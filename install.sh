622a570d8637cbfb97add8e97343f5fe6892523a
negibokken <negibokken@gmail.com>
2020-11-23 22:48:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ ln -s ~/dotfiles/_zshrc ~/.zshrc
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
+
+if [[ -e ~/.vim/colors ]]; then
+  mkdir -p ~/.vim/colors
+fi
+wget https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim -O ~/.vim/colors/hybrid.vim
