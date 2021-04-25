563a9b07d331305e8a399892c35acaeb6eea337b
bokken <negibokken@gmail.com>
2021-04-25 14:43:32 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -s ~/dotfiles/_zshrc ~/.zshrc
+ln -s ~/dotfiles/_zshrc.local ~/.zshrc.local
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
