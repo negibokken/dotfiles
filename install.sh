5d73ed39e4561176f8068ad274be8afa318d518c
Ryo Ogawa <ryo.ogawa@nex.nikkei.com>
2025-01-23 16:32:55 +0900

    update(ghostty): Update install script

--- install.sh
+++ install.sh
@@ -13,21 +13,30 @@ if [[ -e ~/.vim/colors ]]; then
   mkdir -p ~/.vim/colors
 fi
 
+# ghostty
+if ! [[ -e ~/.config/ghostty ]]; then
+  mkdir -p ~/.config/ghostty
+fi
+ln -s ~/dotfiles/ghostty/config ~/.config/ghostty/config
+if [[ -e "$HOME/Library/Application\ Support/com.mitchellh.ghostty" ]]; then
+  ln -s ~/dotfiles/ghostty/config "~/Library/Application\ Support/com.mitchellh.ghostty/"
+fi
+
 if ! [[ -e ~/.vim/colors/hybrid.vim ]]; then
   mkdir -p ~/.vim/colors
   wget https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim -O ~/.vim/colors/hybrid.vim
 fi
 
 if ! [[ -e ~/.vim/after ]]; then
-    mkdir -p ~/.vim/after
-    ln -s ~/dotfiles/vimconf/ftplugin ~/.vim/after/
+  mkdir -p ~/.vim/after
+  ln -s ~/dotfiles/vimconf/ftplugin ~/.vim/after/
 fi
 
 if ! [[ -e ~/.gitconfig.local ]]; then
-    printf "\e[31mPlease fill out ~/.gitconfig.local\e[m\n"
-    printf "[user]\n    name = \n    email = \n" > ~/.gitconfig.local
+  printf "\e[31mPlease fill out ~/.gitconfig.local\e[m\n"
+  printf "[user]\n    name = \n    email = \n" >~/.gitconfig.local
 fi
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
