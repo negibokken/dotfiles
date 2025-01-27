a090f7fd86b18f00f63cb1b705ee1a78db7801ed
Ryo Ogawa <ryo.ogawa@nex.nikkei.com>
2025-01-27 11:58:36 +0900

    fix(nvim): Fix obsidian.nvim option properly

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -78,16 +78,16 @@ return {
 		end,
 
 		preferred_link_style = "wiki",
-	},
 
-	-- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-	-- URL it will be ignored but you can customize this behavior here.
-	---@param url string
-	follow_url_func = function(url)
-		-- Open the URL in the default web browser.
-		-- vim.fn.jobstart({ "open", url }) -- Mac OS
-		-- vim.fn.jobstart({"xdg-open", url})  -- linux
-		-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
-		vim.ui.open(url) -- need Neovim 0.10.0+
-	end,
+		-- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+		-- URL it will be ignored but you can customize this behavior here.
+		---@param url string
+		follow_url_func = function(url)
+			-- Open the URL in the default web browser.
+			-- vim.fn.jobstart({ "open", url }) -- Mac OS
+			-- vim.fn.jobstart({"xdg-open", url})  -- linux
+			-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+			vim.ui.open(url) -- need Neovim 0.10.0+
+		end,
+	},
 }
