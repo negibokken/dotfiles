33f463e42037972cfe72aee6acc0e335e5d28d42
Ryo Ogawa <ryo.ogawa@nex.nikkei.com>
2025-01-27 11:21:16 +0900

    update(nvim): Update nvim config for zettelkasten

--- nvim/lua/config/create-zettelkasten.lua
+++ nvim/lua/config/create-zettelkasten.lua
@@ -6,6 +6,22 @@ _G.create_zettelkasten_note = function()
 	end
 
 	local filename = second_brain .. os.date("/%Y%m%d%H%M%S") .. ".md"
+	local obsidian_path = "/00 --zettelkasten" .. os.date("/%Y%m%d%H%M%S") .. ".md"
+	local link_name = os.date("zettelkasten-%Y%m%d%H%M%S") .. ".md"
+
+	local current_win = vim.api.nvim_get_current_win()
+	local current_buf = vim.api.nvim_get_current_buf()
+	local cursor_pos = vim.api.nvim_win_get_cursor(current_win)
+
+	-- カーソル位置にファイル名を挿入
+	vim.api.nvim_buf_set_text(
+		current_buf,
+		cursor_pos[1] - 1,
+		cursor_pos[2],
+		cursor_pos[1] - 1,
+		cursor_pos[2],
+		{ "[[" .. obsidian_path .. "|" .. link_name .. "]]" }
+	)
 
 	if filename ~= "" then
 		vim.cmd("edit " .. filename)
@@ -13,4 +29,5 @@ _G.create_zettelkasten_note = function()
 end
 
 vim.api.nvim_create_user_command("Zettelkasten", create_zettelkasten_note, { nargs = 0 })
-vim.keymap.set("n", "<A-z>", ":lua create_zettelkasten_note()<CR>:NoNeckPain<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-z>", ":lua create_zettelkasten_note()<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-z>", "<ESC>:lua create_zettelkasten_note()<CR>", { noremap = true, silent = true })
