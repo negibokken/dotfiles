1f49313df946129094e9a09d38ce2a7b415f3405
bokken <negibokken@gmail.com>
2025-01-30 07:11:12 +0900

    fix(nvim): Fix obsidian.nvim config to avoid inserting checkbox

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -25,7 +25,6 @@ return {
 			{
 				name = "personal",
 				path = "~/Documents/obsidian/notes",
-				disable_frontmatter = true,
 			},
 		},
 
@@ -39,29 +38,52 @@ return {
 
 		mappings = {
 			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-			["gf"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			-- Toggle check-boxes.
-			["<leader>ch"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true },
-			},
-			-- Smart action depending on context, either follow link or toggle checkbox.
-			["<cr>"] = {
-				action = function()
-					return require("obsidian").util.smart_action()
-				end,
-				opts = { buffer = true, expr = true },
-			},
+			-- ["gf"] = {
+			-- 	action = function()
+			-- 		return require("obsidian").util.gf_passthrough()
+			-- 	end,
+			-- 	opts = { noremap = false, expr = true, buffer = true },
+			-- },
+			-- -- Toggle check-boxes.
+			-- ["<leader>ch"] = {
+			-- 	action = function()
+			-- 		return require("obsidian").util.toggle_checkbox()
+			-- 	end,
+			-- 	opts = { buffer = true },
+			-- },
+			-- -- Smart action depending on context, either follow link or toggle checkbox.
+			-- ["<cr>"] = {
+			-- 	action = function()
+			-- 		return require("obsidian").util.smart_action()
+			-- 	end,
+			-- 	opts = { buffer = true, expr = true },
+			-- },
 		},
 
 		open_notes_in = "vsplit",
+
+		-- disable_frontmatter = true,
+
+		-- Optional, alternatively you can customize the frontmatter data.
+		---@return table
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+
+			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			return out
+		end,
 
 		-- Optional, customize how wiki links are formatted. You can set this to one of:
 		--  * "use_alias_only", e.g. '[[Foo Bar]]'
