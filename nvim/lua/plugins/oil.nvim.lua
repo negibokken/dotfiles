de0212d733bf78770dbc23f98bdd3a5a001a40ed
bokken <negibokken@gmail.com>
2026-01-02 15:48:54 +0900

    nvim: Add <leader>e keymap for oil.nvim floating window
    - Configure oil.nvim with floating window settings
    - Add <leader>e keybinding to toggle oil file explorer
    - Set border to rounded and add proper padding
    - Restore detailed oil.nvim configuration options

--- nvim/lua/plugins/oil.nvim.lua
+++ nvim/lua/plugins/oil.nvim.lua
@@ -1,76 +1,76 @@
 return {
 	"stevearc/oil.nvim",
-	dependencies = {
-		"nvim-mini/mini.icons",
-	},
-	lazy = false,
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			columns = {
-				"icon",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		default_file_explorer = true,
+		columns = {
+			"icon",
+		},
+		buf_options = {
+			buflisted = false,
+			bufhidden = "hide",
+		},
+		win_options = {
+			wrap = false,
+			signcolumn = "no",
+			cursorcolumn = false,
+			foldcolumn = "0",
+			spell = false,
+			list = false,
+			conceallevel = 3,
+			concealcursor = "nvic",
+		},
+		delete_to_trash = false,
+		skip_confirm_for_simple_edits = false,
+		prompt_save_on_select_new_entry = true,
+		cleanup_delay_ms = 2000,
+		lsp_file_methods = {
+			enabled = true,
+			timeout_ms = 1000,
+			autosave_changes = false,
+		},
+		constrain_cursor = "editable",
+		watch_for_changes = false,
+		view_options = {
+			show_hidden = false,
+			is_hidden_file = function(name, bufnr)
+				local m = name:match("^%.$")
+				return m ~= nil
+			end,
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+			natural_order = "fast",
+			case_insensitive = false,
+			sort = {
+				{ "type", "asc" },
+				{ "name", "asc" },
 			},
-			buf_options = {
-				buflisted = false,
-				bufhidden = "hide",
-			},
+		},
+		float = {
+			padding = 2,
+			max_width = 0,
+			max_height = 0,
+			border = "rounded",
 			win_options = {
-				wrap = false,
-				signcolumn = "no",
-				cursorcolumn = false,
-				foldcolumn = "0",
-				spell = false,
-				list = false,
-				conceallevel = 3,
-				concealcursor = "nvic",
+				winblend = 0,
 			},
-			delete_to_trash = false,
-			skip_confirm_for_simple_edits = false,
-			prompt_save_on_select_new_entry = true,
-			cleanup_delay_ms = 2000,
-			lsp_file_methods = {
-				enabled = true,
-				timeout_ms = 1000,
-				autosave_changes = false,
-			},
-			constrain_cursor = "editable",
-			watch_for_changes = false,
-			keymaps = {
-				["g?"] = { "actions.show_help", mode = "n" },
-				["<CR>"] = "actions.select",
-				["<C-s>"] = { "actions.select", opts = { vertical = true } },
-				["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-				["<C-t>"] = { "actions.select", opts = { tab = true } },
-				["<C-p>"] = "actions.preview",
-				["<C-c>"] = { "actions.close", mode = "n" },
-				["<C-l>"] = "actions.refresh",
-				["-"] = { "actions.parent", mode = "n" },
-				["_"] = { "actions.open_cwd", mode = "n" },
-				["`"] = { "actions.cd", mode = "n" },
-				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-				["gs"] = { "actions.change_sort", mode = "n" },
-				["gx"] = "actions.open_external",
-				["g."] = { "actions.toggle_hidden", mode = "n" },
-				["g\\"] = { "actions.toggle_trash", mode = "n" },
-				["Z"] = false,
-			},
-			use_default_keymaps = true,
-			view_options = {
-				show_hidden = false,
-				is_hidden_file = function(name, bufnr)
-					local m = name:match("^%.$")
-					return m ~= nil
-				end,
-				is_always_hidden = function(name, bufnr)
-					return false
-				end,
-				natural_order = "fast",
-				case_insensitive = false,
-				sort = {
-					{ "type", "asc" },
-					{ "name", "asc" },
-				},
-			},
-		})
-	end,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("oil").toggle_float()
+			end,
+			desc = "Toggle Oil file explorer",
+		},
+	},
 }
+
