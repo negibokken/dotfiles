aa48ec80f0f3f96dcaab231b131abd801491da48
bokken <negibokken@gmail.com>
2026-01-02 10:13:02 +0900

    nvim: Introduce profile plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -86,3 +86,30 @@ vim.keymap.set("i", "<C-e>", 'copilot#Accept("\\<CR>")', {
 	replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
+
+-- [profiel] Enable to profile when NVIM_PROFILE environment variable is set
+local should_profile = os.getenv("NVIM_PROFILE")
+if should_profile then
+	require("profile").instrument_autocmds()
+	if should_profile:lower():match("^start") then
+		require("profile").start("*")
+	else
+		require("profile").instrument("*")
+	end
+end
+
+local function toggle_profile()
+	local prof = require("profile")
+	if prof.is_recording() then
+		prof.stop()
+		vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
+			if filename then
+				prof.export(filename)
+				vim.notify(string.format("Wrote %s", filename))
+			end
+		end)
+	else
+		prof.start("*")
+	end
+end
+vim.keymap.set("", "<f3>", toggle_profile)
