7f313c7bc538f50ac27da92301eb3fa37197f9eb
Ryo Ogawa <ryo.ogawa@nex.nikkei.com>
2026-01-26 11:23:38 +0900

    nvim: Introduce telescope live grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,19 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "master",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-live-grep-args.nvim",
+			-- This will not install any breaking changes.
+			-- For major updates, this must be adjusted manually.
+			version = "^1.0.0",
+		},
+	},
 	config = function()
 		local actions = require("telescope.actions")
-		require("telescope").setup({
+		local telescope = require("telescope")
+		telescope.setup({
 			defaults = {
 				mappings = {
 					i = { -- インサートモードでのキー設定
@@ -18,10 +27,12 @@ return {
 				},
 			},
 		})
+		telescope.load_extension("live_grep_args")
 		-- Telescopeで特定のキーバインドを登録する例
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" }) -- ファイル検索
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" }) -- grep検索
+		-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" }) -- grep検索
+		vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" }) -- バッファ検索
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" }) -- ヘルプ検索
 	end,
